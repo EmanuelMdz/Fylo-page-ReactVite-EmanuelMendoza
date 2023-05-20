0864cde9ecb1cb637ad80fe1fc0b352a1c1a860f
Emanuel Mendoza Mori <130607153+EmanuelMdz@users.noreply.github.com>
2023-05-19 21:07:58 -0300

    Update Index.jsx

--- src/Components/Navbar/Index.jsx
+++ src/Components/Navbar/Index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { Features } from "../Features/Features";
+import Logo from "../../public/icons/logo.svg";
 
 export const Navbar = () => {
   return (
     <nav>
       <div className="nav-logo">
-        <img src="public\icons\logo.svg" alt="" />
+        <img src="Logo" alt="" />
       </div>
       <div className="nav-menu">
         <Link to={"/features"}>Features</Link>
