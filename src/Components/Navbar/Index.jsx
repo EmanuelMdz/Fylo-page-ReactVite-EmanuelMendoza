a161c4a9608d6f2a9fcc0bed9d57b281c3ce5202
Emanuel Mendoza Mori <130607153+EmanuelMdz@users.noreply.github.com>
2023-06-06 11:38:56 -0300

    final Version

--- src/Components/Navbar/Index.jsx
+++ src/Components/Navbar/Index.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { Features } from "../Features/Features";
-import Logo from "../../public/icons/logo.svg";
 
 export const Navbar = () => {
   return (
     <nav>
       <div className="nav-logo">
-        <img src="Logo" alt="" />
+        <img src="public\icons\logo.svg" alt="" />
       </div>
       <div className="nav-menu">
         <Link to={"/features"}>Features</Link>
